8da56fa7a830cd95a3356b1c9118027116fcf91e
Ryan Khetlyr <randallard@yahoo.com>
2024-12-30 17:55:12 -0800

    working connection on windows

--- connect/connect.ino
+++ connect/connect.ino
@@ -1,13 +1,44 @@
 #include <ArduinoBLE.h>
-BLEService newService("180A"); // creating the service
+#include "ArduinoGraphics.h"
+#include "Arduino_LED_Matrix.h"
 
-BLEUnsignedCharCharacteristic randomReading("2A58", BLERead | BLENotify); // creating the Analog Value characteristic
-BLEByteCharacteristic switchChar("2A57", BLERead | BLEWrite); // creating the LED characteristic
+BLEService newService("0000180F-0000-1000-8000-00805F9B34FB");  // Battery Service
+BLEByteCharacteristic switchChar("00002A1B-0000-1000-8000-00805F9B34FB", BLERead | BLEWrite); // Battery State
+
+ArduinoLEDMatrix matrix;
 
 const int ledPin = 2;
 long previousMillis = 0;
-const char* deviceName = "UnoR4-01"; // Store the device name in a variable
+const char* deviceName = "UnoR4-01";
+bool isAnimating = false;
 
+void sayYoureTheGreatest() {
+  matrix.beginDraw();
+  matrix.stroke(0xFFFFFFFF);
+  matrix.textScrollSpeed(50);
+  
+  const char text[] = "    Love you!    ";
+  matrix.textFont(Font_5x7);
+  matrix.beginText(0, 1, 0xFFFFFF);
+  matrix.println(text);
+  matrix.endText(SCROLL_LEFT);
+  
+  matrix.endDraw();
+}
+
+void sayHiBrianna() {
+  matrix.beginDraw();
+  matrix.stroke(0xFFFFFFFF);
+  matrix.textScrollSpeed(50);
+  
+  const char text[] = "    Hi, Brianna!    ";
+  matrix.textFont(Font_5x7);
+  matrix.beginText(0, 1, 0xFFFFFF);
+  matrix.println(text);
+  matrix.endText(SCROLL_LEFT);
+  
+  matrix.endDraw();
+}
 
 void setup() {
   Serial.begin(9600);    
@@ -15,69 +46,71 @@ void setup() {
 
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(ledPin, OUTPUT);
+  
+  matrix.begin();  // Initialize matrix once at startup
 
   if (!BLE.begin()) {
     Serial.println("starting Bluetooth® Low Energy failed!");
     while (1);
   }
 
-  // Set the local name
   BLE.setLocalName(deviceName);
-  
   BLE.setAdvertisedService(newService);
+  
   newService.addCharacteristic(switchChar);
-  newService.addCharacteristic(randomReading);
   BLE.addService(newService);
 
   switchChar.writeValue(0);
-  randomReading.writeValue(0);
 
-  // Set advertising interval
   BLE.setAdvertisingInterval(100);
-  
   BLE.advertise();
+  
   Serial.print("Bluetooth® device '");
   Serial.print(deviceName);
   Serial.println("' active, waiting for connections...");
 }
 
 void loop() {
-  
-  BLEDevice central = BLE.central(); // wait for a Bluetooth® Low Energy central
+  BLEDevice central = BLE.central();
 
-  if (central) {  // if a central is connected to the peripheral
+  if (central) {
     Serial.print("Connected to central: ");
+    Serial.println(central.address());
     
-    Serial.println(central.address()); // print the central's BT address
-    
-    digitalWrite(LED_BUILTIN, HIGH); // turn on the LED to indicate the connection
+    sayHiBrianna();
 
-    // check the battery level every 200ms
-    // while the central is connected:
     while (central.connected()) {
       long currentMillis = millis();
       
-      if (currentMillis - previousMillis >= 200) { // if 200ms have passed, we check the battery level
+      if (currentMillis - previousMillis >= 200) {
         previousMillis = currentMillis;
 
-        int randomValue = analogRead(A1);
-        randomReading.writeValue(randomValue);
-
         if (switchChar.written()) {
-          if (switchChar.value()) {   // any value other than 0
-            Serial.println("LED on");
-            digitalWrite(ledPin, HIGH);         // will turn the LED on
-          } else {                              // a 0 value
-            Serial.println(F("LED off"));
-            digitalWrite(ledPin, LOW);          // will turn the LED off
+          if (switchChar.value()) {
+            Serial.println("Animation start");
+            isAnimating = true;
+            matrix.loadSequence(LEDMATRIX_ANIMATION_HEARTBEAT);
+            matrix.play(true);
+          } else {
+            Serial.println("Animation stop");
+            isAnimating = false;
+            matrix.clear();  // Clear the current animation
+            sayYoureTheGreatest();
           }
         }
-
+        
+        // Keep animation running if active
+        if (isAnimating) {
+          matrix.play(true);
+        }
       }
     }
     
-    digitalWrite(LED_BUILTIN, LOW); // when the central disconnects, turn off the LED
     Serial.print("Disconnected from central: ");
     Serial.println(central.address());
+    
+    // Clean up on disconnect
+    matrix.clear();
+    isAnimating = false;
   }
 }
